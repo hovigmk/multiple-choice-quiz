ffef1b7522bc3d192dcd48a3cd82fb817c7fe52b
Hovig Kalayjian <hovigkalayjian@gmail.com>
2022-11-23 17:47:41 -0500

    uploading the questions

---
+++ questions.js
@@ -0,0 +1,184 @@
+var questions = [
+  {
+    question: "What is JavaScript?",
+    options: [
+      "JavaScript is a scripting language used to make the website interactive",
+      "JavaScript is an assembly language used to make the website interactive",
+      "JavaScript is a compiled language used to make the website interactive",
+      "None of the mentioned",
+    ],
+    correct:
+      "JavaScript is a scripting language used to make the website interactive",
+  },
+  {
+    question: "Which of the following is correct about JavaScript?",
+    options: [
+      "JavaScript is an Object-Based language",
+      "JavaScript is Assembly-language",
+      "JavaScript is an Object-Oriented language",
+      "JavaScript is a High-level language",
+    ],
+    correct: "JavaScript is an Object-Based language",
+  },
+  {
+    question:
+      "Among the given statements, which statement defines closures in JavaScript?",
+    options: [
+      "JavaScript is a function that is enclosed with references to its inner function scope",
+      "JavaScript is a function that is enclosed with references to its lexical environment",
+      "JavaScript is a function that is enclosed with the object to its inner function scope",
+      "None of the mentioned",
+    ],
+    correct:
+      "JavaScript is a function that is enclosed with references to its lexical environment",
+  },
+  {
+    question:
+      "Arrays in JavaScript are defined by which of the following statements?",
+    options: [
+      "It is an ordered list of values",
+      "It is an ordered list of objects",
+      "It is an ordered list of string",
+      "It is an ordered list of functions",
+    ],
+    correct: "It is an ordered list of values",
+  },
+  {
+    question: "Which of the following is not javascript data types?"[
+      ("Null type", "Undefined type", "Number type", "All of the mentioned")
+    ],
+    correct: "All of the mentioned",
+  },
+
+  {
+    question:
+      "Where is Client-side JavaScript code is embedded within HTML documents?",
+    options: [
+      "A URL that uses the special javascript:code",
+      "A URL that uses the special javascript:protocol",
+      "A URL that uses the special javascript:encoding",
+      "A URL that uses the special javascript:stack",
+    ],
+    correct: "A URL that uses the special javascript:protocol",
+  },
+
+  {
+    question:
+      "Which of the following object is the main entry point to all client-side JavaScript features and APIs?",
+    options: ["Position", "Window", "Standard", "Location"],
+    correct: "Window",
+  },
+
+  {
+    question:
+      "Which of the following can be used to call a JavaScript Code Snippet?",
+    options: ["Function/Method", "Preprocessor", "Triggering Event", "RMI"],
+    correct: "Function/Method",
+  },
+
+  {
+    question:
+      "Which of the following explains correctly what happens when a JavaScript program is developed on a Unix Machine?",
+    options: [
+      "will work perfectly well on a Windows Machine",
+      "will be displayed as JavaScript text on the browser",
+      "will throw errors and exceptions",
+      "must be restricted to a Unix Machine only",
+    ],
+    correct: "will work perfectly well on a Windows Machine",
+  },
+
+  {
+    question: "Which of the following scoping type does JavaScript use?",
+    options: ["Sequential", "Segmental", "Lexical", "Literal"],
+    correct: "Lexical",
+  },
+
+  {
+    question: "What is the basic difference between JavaScript and Java?",
+    options: [
+      "Functions are considered as fields",
+      "Functions are values, and there is no hard distinction between methods and fields",
+      "Variables are specific",
+      "There is no difference",
+    ],
+    correct:
+      "Functions are values, and there is no hard distinction between methods and fields",
+  },
+
+  {
+    question: "Why JavaScript Engine is needed?",
+    options: [
+      "Both Compiling & Interpreting the JavaScript",
+      "Parsing the javascript",
+      "Interpreting the JavaScript",
+      "Compiling the JavaScript",
+    ],
+    correct: "Interpreting the JavaScript",
+  },
+
+  {
+    question:
+      "Which of the following methods/operation does javascript use instead of == and !=?",
+    options: [
+      "JavaScript uses equalto()",
+      "JavaScript uses equals() and notequals() instead",
+      "JavaScript uses bitwise checking",
+      "JavaScript uses === and !== instead",
+    ],
+    correct: "JavaScript uses === and !== instead",
+  },
+
+  {
+    question:
+      " What will be the result or type of error if p is not defined in the following JavaScript code snippet? console.log(p)",
+    options: ["Value not found Error", "Reference Error", "Null", "Zero"],
+    correct: "Reference Error",
+  },
+
+  {
+    question:
+      "What is the prototype represents in the following JavaScript code snippet? function javascript() {};",
+    options: [
+      "Not valid",
+      "Prototype of a function",
+      "Function javascript",
+      "A custom constructor",
+    ],
+    correct: "A custom constructor",
+  },
+  {
+    question: "Why event handlers is needed in JS?",
+    options: [
+      "Allows JavaScript code to alter the behaviour of windows",
+      "Adds innerHTML page to the code",
+      "Change the server location",
+      "Performs handling of exceptions and occurrences",
+    ],
+    correct: "Allows JavaScript code to alter the behaviour of windows",
+  },
+
+  {
+    question: "Which of the following is not a framework?",
+    options: [" JavaScript .NET", "JavaScript", "Cocoa JS", "jQuery"],
+    correct: "JavaScript",
+  },
+
+  {
+    question:
+      "Which of the following is the property that is triggered in response to JS errors?",
+    options: ["onclick", "onerror", "onmessage", "onexception"],
+    correct: "onerror",
+  },
+
+  {
+    question: "Which of the following is not an error in JavaScript?",
+    options: [
+      "Missing of Bracket",
+      "Division by zero",
+      "Syntax error",
+      "Missing of semicolons",
+    ],
+    correct: "Division by zero",
+  },
+];
--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const startquiz = document.querySelector(".start-btn button");
+const startquiz = document.getElementById("start_quiz");
 const timerheader = document.querySelector(".timertext");
 
-start_btn.onclick = () => {
+startquiz.addEventListener("click", () => {
   startTimer(); //calling startTimer function
-  startTimerLine(0); //calling startTimerLine function
-};
-(function () {
-  var sec = 60;
-  function startTimer() {
-    console.log("timer suppose to go");
-    var timer = setInterval(function () {
-      sec--;
-      document.getElementById("timerDisplay").innerHTML = "00:" + sec;
-      if (sec < 0) {
-        clearInterval(timer);
-        alert("Time is up!");
-      }
-    }, 1000);
-  }
-  document.getElementById("incorrect").addEventListener("click", function () {
-    sec -= 5;
+});
+
+var sec = 60;
+function startTimer() {
+  console.log("timer suppose to go");
+  var timer = setInterval(function () {
+    sec--;
     document.getElementById("timerDisplay").innerHTML = "00:" + sec;
-  });
-  //startTimer();
-})();
+    if (sec < 0) {
+      clearInterval(timer);
+      alert("Time is up!");
+    }
+  }, 1000);
+}
+document.getElementById("incorrect").addEventListener("click", function () {
+  sec -= 5;
+  document.getElementById("timerDisplay").innerHTML = "00:" + sec;
+});
+
+function showquestions() {}
+//startTimer();
+
 // This function is the end page screen that displays your score after either completeing the quiz or upon timer run out
